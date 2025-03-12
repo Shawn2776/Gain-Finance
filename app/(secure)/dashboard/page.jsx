aaa5f720ab3cd093b3d70994420c4f5d8d006d38
Shawn Harrington <sdharrington@comt-gwkwfc3vjx.clients.nic.edu>
2025-03-12 13:42:00 -0700

    added backend verification to dashbaord

--- app/(secure)/dashboard/page.jsx
+++ app/(secure)/dashboard/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return <div>Sign in to view this page</div>;
+  }
+
+  const user = await currentUser();
+
+  console.log(user);
+
+  return <div>Hello, {user.firstName}</div>;
 };
 
 export default DashboardPage;
