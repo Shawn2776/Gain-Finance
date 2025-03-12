ed11d33b99e0772db87084fade034949a1d7c87b
Shawn Harrington <sdharrington@comt-gwkwfc3vjx.clients.nic.edu>
2025-03-12 13:03:50 -0700

    test change for clerk

--- app/(secure)/dashboard/layout.jsx
+++ app/(secure)/dashboard/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }) => {
   return (
-    <main className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <main className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-100 text-black">
       {children}
     </main>
   );
