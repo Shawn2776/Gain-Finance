5bee5ccdc9f06131a668f5dd02a0137c33637c55
Shawn Harrington <shawn.harrington2776@gmail.com>
2025-03-11 22:02:44 -0700

    init commit - walking through security

---
+++ components/hero/Hero.jsx
@@ -0,0 +1,21 @@
+export default function HeroSection() {
+  return (
+    <section className="w-full flex justify-center mx-auto py-12 md:py-24 lg:py-32 xl:py-48">
+      <div className="">
+        <div className="flex flex-col items-center justify-center space-y-4 text-center">
+          <div className="">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              Welcome to my Channel
+            </h1>
+            <p className="mx-auto w-full text-muted-foreground md:text-xl">
+              Join our community and discover amazing content.
+            </p>
+          </div>
+          <div className="space-x-4">
+            <button className="btn btn-lg">Sign In</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ database/schema.js
@@ -0,0 +1,23 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  clerkId: text("clerk_id").notNull().unique(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
